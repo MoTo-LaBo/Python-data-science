71787b787dbe0698f79b4ca730f0809f74f4e4c4
MoTo <moto.labo.desgin@gmail.com>
2021-09-13 14:14:46 +0900

    NifTi data func

--- CT visualization/ctvis.py
+++ CT visualization/ctvis.py
@@ -1,8 +1,12 @@
+import numpy as np
 import pandas as pd
+import nibabel as nib
 from glob import glob
 
 
+
 # ------------------ DataFrame 作成関数 -------------------
+
 def _get_df(base_path='public-covid-data', folder='ra_im'):
     data_dict = pd.DataFrame({'FilePath': glob('{}/{}/*'.format(base_path, folder)),
                             'FileName': [p.split('/')[-1] for p in glob('{}/{}/*'.format(base_path, folder))]})
@@ -14,4 +18,11 @@ def get_df_all(base_path='public-covid-data'):
     rp_im_df = _get_df(base_path, folder='rp_im')
     rp_msk_df = _get_df(base_path, folder='rp_msk')
     return rp_im_df.merge(rp_msk_df, on='FileName', suffixes=('Image', 'Mask'))
-# ------------------ DataFrame 作成関数 -------------------
+
+
+# ------------------ NiFTI Data 変換関数 -------------------
+
+def load_nifti(path):
+    nifti = nib.load(path)
+    data = nifti.get_fdata()
+    return np.rollaxis(data, 1)
